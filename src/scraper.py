35dc71b077162c76c58fea9da7a2f092ab3c372a
Demir Arikan <dmrarikan@gmail.com>
2024-11-18 10:20:15 -0500

    RSSparser class test

--- src/scraper.py
+++ src/scraper.py
@@ -1,11 +1,37 @@
-import xml
+import re
+from datetime import datetime
+
+import feedparser
 
 
-# TODO: scrape given sites
+class RSSParser:
+    def __init__(self, rss_url: str):
+        self.rss_url = rss_url
+        self.rss_feed = feedparser.parse(rss_url)
+
+    def html_strip(self, html_str: str) -> str:
+        return re.sub(r"<.*?>", "", html_str)
+
+    def get_entries_by_date(self, date: datetime, strip_html=True) -> list:
+        entries = []
+        for entry in self.rss_feed.entries:
+            # published_parsed return 9 tuple in UTC
+            # we get all entries that are published on the same day
+            if (
+                entry.published_parsed[0] == date.year()
+                and entry.published_parsed[1] == date.month()
+                and entry.published_parsed[2] == date.day()
+            ):
+                entries.append(entry)
+        return entries
+    
+    def serialize_to_feed(self, entries: list) -> Feed:
+        
 
 
 
-def scrape_rss(link):
-    pass
-
-
+# will need a better way to save files
+# but a list will do for now
+rss_url = "https://www.ntv.com.tr/gundem.rss"
+feed = feedparser.parse(rss_url)
+print(feed.entries[0].published_parsed[0])
